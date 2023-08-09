4f460ad5dd51aa3520be485ba0639967328dedb4
Keeghan McGarry <keeghan_m@live.com>
2023-08-09 15:14:10 +0100

    Login/Register

--- src/components/app/Main.tsx
+++ src/components/app/Main.tsx
@@ -1,11 +1,34 @@
+import { Show } from 'solid-js'
 import CreateHabit from './CreateHabit'
+import { kinde } from './_auth'
 
 export default function MainApp() {
   return (
-    <div class="h-screen w-screen">
-      <aside class="flex h-screen w-64 flex-col items-center bg-gray-800 py-10 text-gray-200">
-        <CreateHabit />
-      </aside>
+    <div>
+      <Show
+        when={kinde()?.getUser()}
+        fallback={
+          <div class="grid h-screen w-screen place-items-center bg-gray-800 text-white">
+            <div class="flex flex-col items-center justify-center gap-4">
+              <h1 class="text-4xl font-bold">
+                You need to login to use HabitOr
+              </h1>
+              <button
+                onclick={() => kinde().login()}
+                class="rounded-lg bg-gray-600 px-6 py-2 font-bold uppercase text-blue-300 transition-colors duration-300 hover:bg-gray-700 hover:shadow-lg sm:col-span-2"
+              >
+                Login/Register
+              </button>
+            </div>
+          </div>
+        }
+      >
+        <div class="h-screen w-screen">
+          <aside class="flex h-screen w-64 flex-col items-center bg-gray-800 py-10 text-gray-200">
+            <CreateHabit />
+          </aside>
+        </div>
+      </Show>
     </div>
   )
 }
---
+++ src/components/app/_auth.ts
@@ -0,0 +1,12 @@
+import createKindeClient from '@kinde-oss/kinde-auth-pkce-js'
+import { createSignal, onMount } from 'solid-js'
+export const [kinde, setKinde] = createSignal<any>(null)
+
+onMount(async () => {
+  const kinde = await createKindeClient({
+    client_id: import.meta.env.PUBLIC_AUTH_CLIENT_ID,
+    domain: import.meta.env.PUBLIC_AUTH_DOMAIN,
+    redirect_uri: import.meta.env.PUBLIC_AUTH_REDIRECT_URI
+  })
+  setKinde(kinde)
+})
