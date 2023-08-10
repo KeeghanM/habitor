993ed1885d684fb37aedf332e740e2b4e25ad9ae
Keeghan McGarry <keeghan_m@live.com>
2023-08-10 11:39:25 +0100

    Streak

--- src/pages/api/habits/[id]/completionStatus.ts
+++ src/pages/api/habits/[id]/completionStatus.ts
@@ -27,28 +27,31 @@ export const post: APIRoute = async ({ params, request }) => {
 
     const dateString = today.split('T')[0]
     const response = await DB.execute(
-      `SELECT
+      `SELECT 
+    h.id AS habit_id,
     CASE 
         WHEN EXISTS (
             SELECT 1 
             FROM completions c
-            JOIN habits h ON c.habit_id = h.id
-            WHERE c.habit_id = ? AND c.completion_date = ? AND h.user = ?
+            WHERE c.habit_id = h.id AND c.completion_date = CURDATE()
         ) THEN 1 
         ELSE 0 
     END AS is_completed_today,
-    COALESCE(DATEDIFF(CURDATE(), last_missed_date), 0) AS current_streak
-FROM (
-    SELECT 
-        DATE_ADD(MAX(completion_date), INTERVAL 1 DAY) AS last_missed_date
-    FROM completions
-    WHERE habit_id = ? AND completion_date NOT IN (
-        SELECT DATE_ADD(completion_date, INTERVAL 1 DAY) 
-        FROM completions
-        WHERE habit_id = ?
-    )
-) AS sub`,
-      [habit_id, dateString, user_id, habit_id, habit_id]
+    (
+        SELECT COUNT(*)
+        FROM (
+            SELECT hd.date, 
+                   CASE WHEN c.completion_date IS NULL THEN 0 ELSE 1 END AS completed
+            FROM habit_dates hd
+            LEFT JOIN completions c ON hd.habit_id = c.habit_id AND hd.date = c.completion_date
+            WHERE hd.habit_id = h.id
+            ORDER BY hd.date DESC
+        ) AS sub
+        WHERE sub.completed = 1 AND sub.date <= CURDATE()
+    ) AS current_streak
+FROM habits h
+WHERE h.id = ?;`,
+      [habit_id]
     )
 
     const { is_completed_today, current_streak } = response.rows[0] as {
@@ -57,14 +60,12 @@ FROM (
     }
 
     const completed = is_completed_today === '1'
-    const streak = parseInt(current_streak) + (completed ? 1 : 0)
+    const streak = parseInt(current_streak)
 
     console.log({
       habit_id,
       is_completed_today,
-      current_streak,
-      completed,
-      streak
+      current_streak
     })
 
     return new Response(JSON.stringify({ completed, streak }), {
