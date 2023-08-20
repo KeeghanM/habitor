7cf391f861483212986c298031d479dab89d5c04
Keeghan McGarry <keeghan_m@live.com>
2023-08-20 14:20:02 +0100

    Show value

--- src/components/app/Stats/calendar.tsx
+++ src/components/app/Stats/calendar.tsx
@@ -10,12 +10,14 @@ export default function Calendar(props: Props) {
     <div class="grid w-fit grid-cols-7 gap-1">
       {Array.from(Array(daysInMonth).keys()).map((day) => {
         const date = new Date(props.year, props.month - 1, day + 1)
-        const formattedDate = date.toISOString().split('T')[0]
+        const formattedDate = date.toLocaleDateString()
         const statFromThisDay = props.stats?.find(
-          (stat) => stat.completion_date == formattedDate
+          (stat) => stat.completion_date == date.toISOString().split('T')[0]
         )
         const completed = statFromThisDay != undefined
-        const today = new Date().toISOString().split('T')[0] == formattedDate
+        const today =
+          new Date().toISOString().split('T')[0] ==
+          date.toISOString().split('T')[0]
 
         return (
           <div class="group relative cursor-pointer">
@@ -26,8 +28,9 @@ export default function Calendar(props: Props) {
                 (today ? ' border border-white' : '')
               }
             ></div>
-            <div class="pointer-events-none absolute z-20 whitespace-nowrap rounded bg-white px-2 py-1 text-xs text-black opacity-0 group-hover:opacity-100">
-              {formattedDate}
+            <div class="pointer-events-none absolute z-20 flex flex-col gap-2 rounded bg-white px-2 py-1 text-xs text-black opacity-0 group-hover:opacity-100">
+              <p class="whitespace-nowrap">{formattedDate}</p>
+              {statFromThisDay?.value && <p>{statFromThisDay?.value}</p>}
             </div>
           </div>
         )
